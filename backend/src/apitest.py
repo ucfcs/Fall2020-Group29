e50be84fb821bbc853e8c86ba4db42af7900ba1f
Ay-ami <60237407+Ay-ami@users.noreply.github.com>
2020-11-17 20:21:16 -0500

    update w/ rudimentary post

--- backend/src/apitest.py
+++ backend/src/apitest.py
@@ -19,14 +19,30 @@ def index_page():
     return "Home Page"
 
 @app.route('/questions', methods=['GET'])
-def get_questions():
+def get_all_questions():
   questions = mongo.db.questions
   output = []
-  for s in questions.find(): #return all patterns
+  for s in questions.find():
     output.append({'patterns' : s['patterns']})
   for i in questions.find({'context':'CS'}): #only show responses where context is CS
     output.append({'responses' : i['responses']})
   return jsonify({'result' : output})
 
+
+
+newPattern= "how do I post things"
+newResponse = "like this"
+newContext = ["general", "flask"]
+
+@app.route('/questions', methods=['POST'])
+def add_questions():
+  questions = mongo.db.questions
+  #pattern = request.json['pattern']
+  #distance = request.json['distance']
+  questions_id = questions.insert({'pattern': newPattern, 'response': newResponse, 'context':newContext})
+  new_questions = questions.find_one({'_id': questions_id })
+  output = {'pattern' : new_questions['pattern'], 'response' : new_questions['response'], 'context':new_questions['context']}
+  return jsonify({'result' : output})
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) #run in debug mode so that when the .py updates, the whole thing relaunches too
