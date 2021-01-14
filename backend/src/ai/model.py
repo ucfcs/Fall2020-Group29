5bac1a9d498227f0f22ad75fe7e858155bf12d0e
Carlos Santiago Bañón <carlossbanon@gmail.com>
2021-01-13 19:09:53 -0500

    Add comments to the init() and forward() functions.

--- backend/src/ai/model.py
+++ backend/src/ai/model.py
@@ -5,12 +5,26 @@ import torch.nn as nn
 class ChatNeuralNet(nn.Module):
 
     def __init__(self, in_size, hidden_size, num_classes):
+        """
+        Defines the architecture of the neural network according to the parameters.py
+
+        :in_size: the number of nodes in the input layer.
+        :hidden_size: the number of nodes in the hidden layer.
+        :num_classes: the number of classes for the output layer.
+        """
+
         super(ChatNeuralNet, self).__init__()
         self.l1 = nn.Linear(in_size, hidden_size) 
         self.l2 = nn.Linear(hidden_size, hidden_size) 
         self.l3 = nn.Linear(hidden_size, num_classes)
 
     def forward(self, X):
+        """
+        Performs forward propagation through the neural network.
+        
+        :X: the current state of the neural network.
+        """
+
         X = self.l1(X)
         X = self.relu(X)
         X = self.l2(X)
