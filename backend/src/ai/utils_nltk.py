0f60e6c7e788c404a7d0121b7322e0707c16bc4c
Carlos Santiago Bañón <carlossbanon@gmail.com>
2021-01-13 19:25:41 -0500

    Define the bag_of_words() function

--- backend/src/ai/utils_nltk.py
+++ backend/src/ai/utils_nltk.py
@@ -3,6 +3,18 @@ import numpy as np
 from nltk.stem.porter import PorterStemmer
 
 
+def bag_of_words(tokenized_string, words):
+    num_words = len(words)
+    string_words = [stem(word) for word in tokenized_string]
+    bag = np.zeros(num_words, dtype=np.float32)
+
+    for i, word in enumerate(words):
+        if word in string_words: 
+            bag[i] = 1
+
+    return bag
+    
+
 def stem(word):
     stemmer = PorterStemmer()
     return stemmer.stem(word.lower())
