6b627e58be81718123362b70a99026ac1f33ca45
Carlos Santiago Bañón <carlossbanon@gmail.com>
2021-01-13 19:21:04 -0500

    Define the stem() function

--- backend/src/ai/utils_nltk.py
+++ backend/src/ai/utils_nltk.py
@@ -1,5 +1,11 @@
 import nltk
 import numpy as np
+from nltk.stem.porter import PorterStemmer
+
+
+def stem(word):
+    stemmer = PorterStemmer()
+    return stemmer.stem(word.lower())
 
 
 def tokenize(string):
