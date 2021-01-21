615b632fd6341558011b08967844f3de4f4d7cd5
Carlos Santiago Bañón <carlossbanon@gmail.com>
2021-01-21 11:28:14 -0500

    Load the NN configuration

--- backend/src/ai/chatbot.py
+++ backend/src/ai/chatbot.py
@@ -12,3 +12,10 @@ with open('intents.json', 'r') as json_data:
 
 FILE = "data.pth"
 data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+num_classes = data["num_classes"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
