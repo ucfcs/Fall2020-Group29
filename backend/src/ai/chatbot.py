5a9c14af871b6dad96e1896052a6766e807c55d1
Carlos Santiago Bañón <carlossbanon@gmail.com>
2021-01-21 11:26:58 -0500

    Add functionality to read JSON and trained NNs

--- backend/src/ai/chatbot.py
+++ backend/src/ai/chatbot.py
@@ -6,3 +6,9 @@ from utils_nltk import bag_of_words, tokenize
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('intents.json', 'r') as json_data:
+    intents = json.load(json_data)
+
+FILE = "data.pth"
+data = torch.load(FILE)
