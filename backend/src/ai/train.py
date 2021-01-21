66f7aaa07229980deb631e926eb7c789e2c56cfc
Carlos Santiago Bañón <carlossbanon@gmail.com>
2021-01-21 11:46:20 -0500

    Set the training data

--- backend/src/ai/train.py
+++ backend/src/ai/train.py
@@ -30,3 +30,20 @@ ignore_words = ['?', '.', '!']
 all_words = [stem(w) for w in all_words if w not in ignore_words]
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
+
+print(len(xy), "patterns")
+print(len(tags), "tags:", tags)
+print(len(all_words), "unique stemmed words:", all_words)
+
+X_train = []
+y_train = []
+
+for (pattern_sentence, tag) in xy:
+
+    bag = bag_of_words(pattern_sentence, all_words)
+    X_train.append(bag)
+    label = tags.index(tag)
+    y_train.append(label)
+
+X_train = np.array(X_train)
+y_train = np.array(y_train)
