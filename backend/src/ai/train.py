5f495883f23b5d59730b363b4fb7861a065d2b03
Carlos Santiago Bañón <carlossbanon@gmail.com>
2021-01-21 11:45:11 -0500

    Define the intent gathering loop

--- backend/src/ai/train.py
+++ backend/src/ai/train.py
@@ -10,3 +10,18 @@ from utils_nltk import bag_of_words, tokenize, stem
 
 with open('intents.json', 'r') as f:
     intents = json.load(f)
+
+all_words = []
+tags = []
+xy = []
+
+for intent in intents['intents']:
+    
+    tag = intent['tag']
+
+    tags.append(tag)
+    for pattern in intent['patterns']:
+
+        w = tokenize(pattern)
+        all_words.extend(w)
+        xy.append((w, tag))
