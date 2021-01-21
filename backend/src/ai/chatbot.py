38f2289cfb9daebeb9adfc34372602be80fba449
Carlos Santiago Bañón <carlossbanon@gmail.com>
2021-01-21 11:33:20 -0500

    Define the chatbot interaction loop

--- backend/src/ai/chatbot.py
+++ backend/src/ai/chatbot.py
@@ -26,3 +26,29 @@ model.eval()
 
 bot_name = "Knugget"
 print("It me, Knugget! Need some knugvising? (Type 'quit' to exit.)")
+
+while True:
+
+    sentence = input("You: ")
+
+    if sentence == "quit":
+        break
+
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                print(f"{bot_name}: {random.choice(intent['responses'])}")
+    else:
+        print(f"{bot_name}: I do not understand...")
