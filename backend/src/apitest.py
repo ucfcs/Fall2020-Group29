e07466e54888c00eff3e7fe1aa086a464bf7882b
Ay-ami <60237407+Ay-ami@users.noreply.github.com>
2021-01-22 22:55:45 -0500

    Update apitest.py
    adds two PUT methods to update existing questions with forms or contacts respectively

--- backend/src/apitest.py
+++ backend/src/apitest.py
@@ -13,7 +13,7 @@ from flask import jsonify
 from flask import request
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
-import json
+from pymongo import ReturnDocument # so that we can return the updated version of the document after updating it
 
 app = Flask(__name__)
 
@@ -31,18 +31,17 @@ def index_page():
     return "Home Page"
 
 
-@app.route('/get_question', methods=['GET'])
+@app.route('/get_question', methods=['GET']) # retrive a question based on entities
 def get_question(entities = ['BS-to-MS', 'How', 'Sign Up']):
   found = mongo.db.questions.find_one({'entities': { '$all': entities }}) #finds the entry with the exact set of entities 
 
-  fickleID = found.pop('_id') # jasonify doens't know how to handle objects of type ObjectID, so we remove it
+  fickleID = found.pop('_id') # jasonify() doens't know how to handle objects of type ObjectID, so we remove it
   found.update({'_id': str(fickleID)}) # put _id back in but as a regular string now
   
   return jsonify(found) #return result as json
 
 
-
-@app.route('/add_question', methods=['POST'])
+@app.route('/add_question', methods=['POST']) # add a question to the database with name, response, an entites
 def add_question(name = "BS-to-MS", response = "In order to dah da da da da", entities = ['BS-to-MS', 'How', 'Sign Up'] ):
   questions = mongo.db.questions
   questions_id = questions.insert({'name': name, 'responses': response, 'entities':entities})
@@ -51,13 +50,47 @@ def add_question(name = "BS-to-MS", response = "In order to dah da da da da", en
   return jsonify(output)
 
 
-'''
-@app.route('/add_file', methods=['PUT'])
+@app.route('/add_file', methods=['PUT']) # give an existing question a file, returns updated document
 def add_file(entities = ['BS-to-MS', 'How', 'Sign Up'], file = 'https://www.cs.ucf.edu/wp-content/uploads/2020/04/CSIT-Elective-List-AY2020-2021.pdf' ):
-  questions = mongo.db.questions
-  new_questions = questions.update({'Entities': { '$all': entities }, '$set':{'file':file}})
-  output = {'Name' : new_questions['Name'], 'response' : new_questions['response'], 'entities':new_questions['entities'], 'file':file}
-  return jsonify({'result' : output})
-'''
+  updated = mongo.db.questions.find_one_and_update(
+    {
+      'entities': { '$all': entities }
+    }, 
+    {
+      '$set': { 'file':file } 
+    },
+    upsert=True, # upsert = if thing does not exist, make it exist
+    return_document=ReturnDocument.AFTER # need this or else it returns the document from before the update
+    )
+
+  fickleID = updated.pop('_id') # jasonify() doens't know how to handle objects of type ObjectID, so we remove it
+  updated.update({'_id': str(fickleID)}) # put _id back in but as a regular string now
+
+  return jsonify(updated)
+
+
+@app.route('/add_contact', methods=['PUT']) # give an existing question a contact, returns updated document
+def add_file(entities = ['BS-to-MS', 'How', 'Sign Up'], contact = 'heinrich@cs.ucf.edu' ):
+  updated = mongo.db.questions.find_one_and_update(
+    {
+      'entities': { '$all': entities }
+    }, 
+    {
+      '$set': { 'comtact':contact } 
+    },
+    upsert=True, # upsert = if thing does not exist, make it exist
+    return_document=ReturnDocument.AFTER # need this or else it returns the document from before the update
+    )
+
+  fickleID = updated.pop('_id') # jasonify() doens't know how to handle objects of type ObjectID, so we remove it
+  updated.update({'_id': str(fickleID)}) # put _id back in but as a regular string now
+
+  return jsonify(updated)
+
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True) #run in debug mode so that when the .py updates, the whole thing relaunches too
