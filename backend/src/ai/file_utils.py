8fdf29c76032a97ec87f0fbce4a95b0ff02b47c3
Carlos Santiago Bañón <carlossbanon@gmail.com>
2021-02-12 16:20:42 -0500

    Allow import_csv() to read a csv and convert to dict

--- backend/src/ai/file_utils.py
+++ backend/src/ai/file_utils.py
@@ -8,4 +8,13 @@ import pandas as pd
 def import_csv(file_name):
     
     file_data = pd.read_csv(file_name)
+    print(file_data)
+
+    group = file_data.groupby('entity')
+    df2 = group.apply(lambda x: x['patterns'].unique())
+
+    print(df2)
+
+    df3 = df2.to_dict()
+    print(df3)
     
