de7e9e691e5cfc08b6f4dbed417acf6d835720a0
Carlos Santiago Bañón <carlossbanon@gmail.com>
2021-02-16 17:28:52 -0500

    Complete prediction pipeline

---
+++ backend/ai/chatbot.py
@@ -0,0 +1,82 @@
+import json
+import torch
+import numpy as np
+import pandas as pd
+from model import NeuralNet
+from utils import bag_of_words, tokenize
+
+
+def predict_tags(device, file_name, trained_model, utterance):
+
+    # Read the data.
+    data = pd.read_csv(file_name)
+
+    # Load the network architecture.
+    trained_model = torch.load(trained_model)
+
+    # Set the network architecture.
+    input_size = trained_model["input_size"]
+    hidden_size = trained_model["hidden_size"]
+    num_classes = trained_model["num_classes"]
+    all_words = trained_model['all_words']
+    tags = trained_model['tags']
+    model_state = trained_model["model_state"]
+
+    # Load the trained network.
+    model = NeuralNet(input_size, hidden_size, num_classes).to(device)
+    model.load_state_dict(model_state)
+    model.eval()
+
+    # Tokenize the input.
+    utterance = tokenize(utterance)
+    X = bag_of_words(utterance, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    # Predict the user intent using the network.
+    output = model(X)
+    _, top_predictions = torch.topk(output, 2, dim=1)
+    top_predictions = top_predictions.numpy()[0]
+
+    # Determine the prediction probabilities.
+    probs = torch.softmax(output, dim=1)
+    probs = probs.detach().numpy()[0]
+
+    all_predictions = {}
+    
+    # Get the top tags.
+    for i in range(top_predictions.size):
+        tag_prediction = {}
+        tag_prediction['tag'] = tags[top_predictions[i]]
+        tag_prediction['prob'] = probs[top_predictions[i]]
+        all_predictions[i] = tag_prediction
+
+    return all_predictions
+
+
+def predict(utterance):
+
+    # Load the configuration.
+    params_file = 'params.json'
+    with open(params_file) as f:
+        params = json.load(f)
+
+    predicted_tags = {}
+
+    # Set the device to a GPU if available.
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Open and load the intents.
+    file_dept = params['file_dept']
+    file_cat = params['file_cat']
+    file_info = params['file_info']
+
+    tags_dept = predict_tags(device, file_dept, 'trained_model_dept.pth', utterance)
+    tags_cat = predict_tags(device, file_cat, 'trained_model_cat.pth', utterance)
+    tags_info = predict_tags(device, file_info, 'trained_model_info.pth', utterance)
+
+    predicted_tags['dept'] = tags_dept
+    predicted_tags['cat'] = tags_cat
+    predicted_tags['info'] = tags_info
+
+    return predicted_tags
