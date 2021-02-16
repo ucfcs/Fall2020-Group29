60163b0c853d1335b54cbcfa81aa46b5eca2a386
Carlos Santiago Bañón <carlossbanon@gmail.com>
2021-02-16 16:00:17 -0500

    Finish training and evaluation loops

--- backend/ai/train.py
+++ backend/ai/train.py
@@ -5,8 +5,10 @@ import pandas as pd
 import torch
 import torch.nn as nn
 from dataset import ChatDataset
+from evaluate import evaluate
 from model import NeuralNet
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import normalize
 from torch.utils.data import DataLoader
 from utils import bag_of_words, lemmatize, stem, tf_idf, tokenize
 
@@ -53,8 +55,11 @@ def preprocess(data):
     X = np.array(X)
     y = np.array(y)
 
+    # Normalize.
+    X = normalize(X, norm='l2')
+
     # Shuffle and split the data.
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # Show the dimensionality of the data.
     print("X_train:", X_train.shape)
@@ -67,6 +72,97 @@ def preprocess(data):
     return X_train, X_test, y_train, y_test, num_classes, all_words, tags
 
 
+def train_model(data, params):
+    # Preprocess the data.
+    X_train, X_test, y_train, y_test, num_classes, all_words, tags = preprocess(data)
+
+    # Set the parameters.
+    num_epochs = params['num_epochs']
+    batch_size = params['batch_size']
+    learning_rate = params['learning_rate']
+    hidden_size = params['hidden_size']
+
+    # Set the input size.
+    input_size = len(X_train[0])
+    print("NN Input/Output:", input_size, num_classes)
+
+    # Set the dataset.
+    dataset = ChatDataset(X_train, y_train)
+
+    # Set the dataloader.
+    train_loader = DataLoader(dataset=dataset,
+                                batch_size=batch_size,
+                                shuffle=True,
+                                num_workers=0)
+
+    # Set the device to a GPU if available.
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Define the model.
+    model = NeuralNet(input_size, hidden_size, num_classes).to(device)
+
+    # Set the loss function and optimizer.
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    loss_list = np.zeros(num_epochs)
+
+    for epoch in range(num_epochs):
+
+        for (words, labels) in train_loader:
+
+            words = words.to(device)
+            labels = labels.to(dtype=torch.long).to(device)
+
+            # Perform forward propagation.
+            outputs = model(words)
+            loss = criterion(outputs, labels)
+
+            # Perform backpropagation.
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        # Report the loss.
+        if (epoch+1) % 10 == 0:
+            print(f'Epoch ({epoch+1} / {num_epochs}), Loss: {loss.item():.4f}')
+
+            loss_list[epoch] = loss
+
+    # Report the final loss.
+    print(f'final loss: {loss.item():.4f}')
+
+    # Plot the training loss.
+    fig = plt.figure()
+    ax = plt.axes()
+    x = np.arange(1, num_epochs + 1, 1)
+    plt.title('Loss Per Epoch')
+    ax.set_xlabel('Epoch')
+    ax.set_ylabel('Loss')
+    ax.plot(x, loss_list)
+    plt.show()
+
+    # Evaluate the model.
+    evaluate(model, X_test, y_test, tags)
+
+    # Save the model components.
+    data = {
+        "model_state": model.state_dict(),
+        "input_size": input_size,
+        "hidden_size": hidden_size,
+        "num_classes": num_classes,
+        "all_words": all_words,
+        "tags": tags
+    }
+
+    # Save the model to "trained_model.pth".
+    FILE = 'trained_model.pth'
+    torch.save(data, FILE)
+
+    # Report completion of training.
+    print(f'Training complete. File saved to {FILE}.')
+
+
 def train():
 
     params_file = 'params.json'
@@ -76,112 +172,33 @@ def train():
     FLAGS = params['FLAGS']
 
     if FLAGS['int'] == 0 and FLAGS['dept'] == 0 and FLAGS['cat'] == 0 and FLAGS['info'] == 0:
-        print("No training done.")
+        print("No training required.")
         return
 
     if FLAGS['int'] == 1:
         file_name = params['file_int']
-        return
+        data = pd.read_csv(file_name)
+        print("Training Intents")
+        train_model(data, params)
 
     if FLAGS['dept'] == 1:
         file_name = params['file_dept']
-        return
+        data = pd.read_csv(file_name)
+        print("Training Entities: Departments")
+        train_model(data, params)
 
     if FLAGS['cat'] == 1:
+        # Import the data.
         file_name = params['file_cat']
-        return
+        data = pd.read_csv(file_name)
+        print("Training Entities: Categories")
+        train_model(data, params)
 
     if FLAGS['info'] == 1:
-
         # Import the data.
         file_name = params['file_info']
         data = pd.read_csv(file_name)
-
-        # Preprocess the data.
-        X_train, X_test, y_train, y_test, num_classes, all_words, tags = preprocess(data)
-
-        # Set the parameters.
-        num_epochs = params['num_epochs']
-        batch_size = params['batch_size']
-        learning_rate = params['learning_rate']
-        hidden_size = params['hidden_size']
-
-        # Set the input size.
-        input_size = len(X_train[0])
-        print("NN Input/Output:", input_size, num_classes)
-
-        # Set the dataset.
-        dataset = ChatDataset(X_train, y_train)
-
-        # Set the dataloader.
-        train_loader = DataLoader(dataset=dataset,
-                                    batch_size=batch_size,
-                                    shuffle=True,
-                                    num_workers=0)
-
-        # Set the device to a GPU if available.
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-        # Define the model.
-        model = NeuralNet(input_size, hidden_size, num_classes).to(device)
-
-        # Set the loss function and optimizer.
-        criterion = nn.CrossEntropyLoss()
-        optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
-        loss_list = np.zeros(num_epochs)
-
-        for epoch in range(num_epochs):
-
-            for (words, labels) in train_loader:
-
-                words = words.to(device)
-                labels = labels.to(dtype=torch.long).to(device)
-
-                # Perform forward propagation.
-                outputs = model(words)
-                loss = criterion(outputs, labels)
-
-                # Perform backpropagation.
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
-
-            # Report the loss.
-            if (epoch+1) % 10 == 0:
-                print(f'Epoch ({epoch+1} / {num_epochs}), Loss: {loss.item():.4f}')
-
-                loss_list[epoch] = loss
-
-        # Report the final loss.
-        print(f'final loss: {loss.item():.4f}')
-
-        # Plot the training loss.
-        fig = plt.figure()
-        ax = plt.axes()
-        x = np.arange(1, num_epochs + 1, 1)
-        plt.title('Loss Per Epoch')
-        ax.set_xlabel('Epoch')
-        ax.set_ylabel('Loss')
-        ax.plot(x, loss_list)
-        plt.show()
-
-        # Save the model components.
-        data = {
-            "model_state": model.state_dict(),
-            "input_size": input_size,
-            "hidden_size": hidden_size,
-            "num_classes": num_classes,
-            "all_words": all_words,
-            "tags": tags
-        }
-
-        # Save the model to "trained_model.pth".
-        FILE = 'trained_model.pth'
-        torch.save(data, FILE)
-
-        # Report completion of training.
-        print(f'Training complete. File saved to {FILE}.')
-
-
+        print("Training Entities: Information")
+        train_model(data, params)
+        
 train()
