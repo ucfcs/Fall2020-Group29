ea387ad04a774c46bdcb0575e900a78f73ca8259
Carlos Santiago Bañón <carlossbanon@gmail.com>
2021-02-16 11:37:49 -0500

    Rename config.json to params.json

--- backend/ai/params.py
+++
@@ -1,5 +0,0 @@
-file_names = {'file_intents': 'intents.csv',
-          'file_dept': 'entities_dept.csv',
-          'file_cat': 'entities_cat.csv',
-          'file_info': 'entities_info.csv'
-         }
