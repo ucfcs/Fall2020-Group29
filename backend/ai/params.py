80a7236283d8f40f96e85952d78fe78b77d4cfd7
Carlos Santiago Bañón <carlossbanon@gmail.com>
2021-02-16 11:35:32 -0500

    Delete params.py

--- backend/ai/params.py
+++ backend/ai/params.py
@@ -1,4 +1,4 @@
-PARAMS = {'file_intents': 'intents.csv',
+file_names = {'file_intents': 'intents.csv',
           'file_dept': 'entities_dept.csv',
           'file_cat': 'entities_cat.csv',
           'file_info': 'entities_info.csv'
