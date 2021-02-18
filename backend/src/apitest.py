6f613a9de460376d6cf43b966701a2ea81689dad
Ay-ami <60237407+Ay-ami@users.noreply.github.com>
2021-02-18 13:49:33 -0500

    adds todos and standardizes some syntax/origanizations

--- backend/src/apitest.py
+++ backend/src/apitest.py
@@ -1,4 +1,4 @@
-#################
+#################C:\Users\Maya\Documents\projects\flask-app\venv\Scripts\activate
 # > venv\Scripts\activate
 # (venv) > python apitest.py
 # Postman http://localhost:5000/questions
@@ -31,8 +31,20 @@ def index_page():
     return "Home Page"
 
 ### questions collection ###
+# add 
+@app.route('/add_question', methods=['POST']) # add a question to the database with Name, Responses, an Entities, returns this new document
+def add_question( Name = "BS-to-MS", Responses = ["In order to dah da da da da"], Entities = ['BS-to-MS', 'How', 'Sign Up'] ):
+  new_question = {'Name': Name, 'Responses': Responses, 'Entities':Entities}
+  # insert_one() doesn't return a document, it returns a result that contains the ObjectID
+  InsertOneResult_Obj = mongo.db.questions.insert_one(new_question) 
+  # append new_question with the ObjectID (as a string) so that we can actually return something that resembles a document :/
+  new_question.update({'_id':str(InsertOneResult_Obj.inserted_id)}) 
+ 
+  return jsonify(new_question)
+
+# retrive
 @app.route('/get_question', methods=['GET']) # retrive a question based on Entities
-def get_question(Entities = ['BS-to-MS', 'How', 'Sign Up']):
+def get_question( Entities = ['BS-to-MS', 'How', 'Sign Up'] ):
   found = mongo.db.questions.find_one({'Entities': { '$all': Entities }}) #finds the entry with the exact set of Entities 
 
   if (found is None): # if there is no match
@@ -43,18 +55,11 @@ def get_question(Entities = ['BS-to-MS', 'How', 'Sign Up']):
   
   return jsonify(found) #return result as json
 
+# TODO: search questions/responses for a word
 
-@app.route('/add_question', methods=['POST']) # add a question to the database with Name, Responses, an Entities, returns this new document
-def add_question(Name = "BS-to-MS", Responses = ["In order to dah da da da da"], Entities = ['BS-to-MS', 'How', 'Sign Up'] ):
-  new_question = {'Name': Name, 'Responses': Responses, 'Entities':Entities}
-  InsertOneResult_Obj = mongo.db.questions.insert_one(new_question) # insert_one() doesn't return a document, it returns a result that contains the ObjectID
-  new_question.update({'_id':str(InsertOneResult_Obj.inserted_id)}) # append new_question with the ObjectID (as a string) so that we can actually return something that resembles a document :/
- 
-  return jsonify(new_question)
-
-
+# update
 @app.route('/update_question', methods=['PUT']) # give an existing question a file, returns updated document
-def update_question(Entities = ['BS-to-MS', 'How', 'Sign Up'], itemToUpdate = 'Name', newContents='why hello there' ):
+def update_question(Entities = ['BS-to-MS', 'How', 'Sign Up'], itemToUpdate = 'Name', newContents = 'why hello there' ):
   updated = mongo.db.questions.find_one_and_update(
     {
       'Entities': { '$all': Entities }
@@ -73,15 +78,15 @@ def update_question(Entities = ['BS-to-MS', 'How', 'Sign Up'], itemToUpdate = 'N
 
   return jsonify(updated)
 
-
+# append existing question with a file
 @app.route('/put_file', methods=['PUT']) # give an existing question a file, returns updated document
-def put_file(Entities = ['BS-to-MS', 'How', 'Sign Up'], file = 'https://www.cs.ucf.edu/wp-content/uploads/2020/04/CSIT-Elective-List-AY2020-2021.pdf' ):
+def put_file( Entities = ['BS-to-MS', 'How', 'Sign Up'], file = 'https://www.cs.ucf.edu/wp-content/uploads/2020/04/CSIT-Elective-List-AY2020-2021.pdf' ):
   updated = mongo.db.questions.find_one_and_update(
     {
       'Entities': { '$all': Entities }
     }, 
     {
-      '$set': { 'file':file } 
+      '$set': { 'File':file } 
     },
     upsert=True, # upsert = if thing does not exist, make it exist
     return_document=ReturnDocument.AFTER # need this or else it returns the document from before the update
@@ -93,7 +98,7 @@ def put_file(Entities = ['BS-to-MS', 'How', 'Sign Up'], file = 'https://www.cs.u
 
   return jsonify(updated)
 
-
+# append existing question with a contact
 @app.route('/put_contact', methods=['PUT']) # give an existing question a contact, returns updated document
 def put_contact(Entities = ['BS-to-MS', 'How', 'Sign Up'], contact = 'heinrich@cs.ucf.edu' ):
   updated = mongo.db.questions.find_one_and_update(
@@ -101,7 +106,7 @@ def put_contact(Entities = ['BS-to-MS', 'How', 'Sign Up'], contact = 'heinrich@c
       'Entities': { '$all': Entities }
     }, 
     {
-      '$set': { 'contact':contact } 
+      '$set': { 'Contact':contact } 
     },
     upsert=True, # upsert = if thing does not exist, make it exist
     return_document=ReturnDocument.AFTER # need this or else it returns the document from before the update
@@ -114,19 +119,37 @@ def put_contact(Entities = ['BS-to-MS', 'How', 'Sign Up'], contact = 'heinrich@c
 
   return jsonify(updated)
 
+  # TODO: append an experation date to a question?
+
+#delete
+@app.route('/delete_question', methods=['DELETE']) # retrive a question based on Entities
+def get_question(Entities = ['BS-to-MS', 'How', 'Sign Up']):
+  found = mongo.db.questions.find_one({'Entities':{ '$all': Entities }}) #finds the entry with the exact set of Entities 
+
+  if (found is None): # if there is no match
+    return jsonify({'result':'no match'})
+
+  fickleID = found.pop('_id') # jasonify() doens't know how to handle objects of type ObjectID, so we remove it
+  found.update({'_id':str(fickleID)}) # put _id back in but as a regular string now
+  
+  return jsonify(found) #return result as json
+
 
 ### contacts collection ###
+# add
 @app.route('/add_contact', methods=['POST']) # add a contact to the database with Name, Title, Email, Phone and Office, returns inerted doctment 
-def add_contact(Name = 'Mark Heinrich', Title='CS Advisor', Email='heinrich@cs.ucf.edu', Phone='(407) 882-0138', Office='HEC 345'):
-  new_contact = {'Name': Name, 'Title': Title, 'Email':Email, 'Phone':Phone, 'Office':Office}
+def add_contact(Name = 'Mark Heinrich', Title = 'CS Advisor', Email = 'heinrich@cs.ucf.edu', Phone = '(407) 882-0138', Office = 'HEC 345'):
+  new_contact = {'Name':Name, 'Title':Title, 'Email':Email, 'Phone':Phone, 'Office':Office}
   InsertOneResult_Obj = mongo.db.contacts.insert_one(new_contact) # insert_one() doesn't return a document, it returns a result that contains the ObjectID
   new_contact.update({'_id':str(InsertOneResult_Obj.inserted_id)}) # append new_contact with the ObjectID (as a string) so that we can actually return something that resembles a document :/
  
   return jsonify(new_contact)
 
+# TODO: search
 
+# update
 @app.route('/update_contact', methods=['PUT']) # give an existing contact a file, returns updated document
-def update_contact(Name = 'Mark Heinrich', Title='CS Advisor', itemToUpdate = 'Name', newContents='Mark Heinrich 2' ):
+def update_contact(Name = 'Mark Heinrich', Title = 'CS Advisor', itemToUpdate = 'Name', newContents = 'Mark Heinrich 2' ):
   updated = mongo.db.contacts.find_one_and_update(
     {
       '$and': [
@@ -144,11 +167,46 @@ def update_contact(Name = 'Mark Heinrich', Title='CS Advisor', itemToUpdate = 'N
     return jsonify({'result':'no match'})
 
   fickleID = updated.pop('_id') # jasonify() doens't know how to handle objects of type ObjectID, so we remove it
-  updated.update({'_id': str(fickleID)}) # put _id back in but as a regular string now
+  updated.update({'_id':str(fickleID)}) # put _id back in but as a regular string now
 
   return jsonify(updated)
 
+# TODO: delete
 
+
+## entities collection ##
+
+# TODO: create 
+
+# TODO: search entity
+
+# TODO: search a triplet of entities
+
+# TODO: update
+
+# TODO: delete
+
+
+## files collection ##
+
+# TODO: create 
+
+# TODO: search files
+
+# TODO: update
+
+# TODO: delete
+
+
+## users collection ##
+
+# TODO: create 
+
+# TODO: search
+
+# TODO: update
+
+# TODO: delete
 
 
 
