4dc0c7975d87e75e3e18425c4066811409342457
Carlos Santiago Bañón <carlossbanon@gmail.com>
2021-03-10 21:55:13 -0500

    Make a new 'num_predictions' parameter

--- ai/chatbot.py
+++ ai/chatbot.py
@@ -6,7 +6,7 @@ from model import NeuralNet
 from utils import bag_of_words, tokenize
 
 
-def predict_tags(device, file_name, trained_model, utterance):
+def predict_tags(device, file_name, trained_model, utterance, num_predictions):
 
     # Read the data.
     data = pd.read_csv(file_name)
@@ -34,9 +34,8 @@ def predict_tags(device, file_name, trained_model, utterance):
     X = torch.from_numpy(X).to(device)
 
     # Predict the user intent using the network.
-    num_preds = 1
     output = model(X)
-    _, top_predictions = torch.topk(output, num_preds, dim=1)
+    _, top_predictions = torch.topk(output, num_predictions, dim=1)
     top_predictions = top_predictions.numpy()[0]
 
     # Determine the prediction probabilities.
@@ -72,11 +71,12 @@ def predict(utterance):
     file_dept = params['file_dept']
     file_cat = params['file_cat']
     file_info = params['file_info']
+    num_predictions = params['num_predictions']
 
-    tags_ints = predict_tags(device, file_ints, 'models/trained_model_ints.pth', utterance)
-    tags_dept = predict_tags(device, file_dept, 'models/trained_model_dept.pth', utterance)
-    tags_cat = predict_tags(device, file_cat, 'models/trained_model_cat.pth', utterance)
-    tags_info = predict_tags(device, file_info, 'models/trained_model_info.pth', utterance)
+    tags_ints = predict_tags(device, file_ints, 'models/trained_model_ints.pth', utterance, num_predictions)
+    tags_dept = predict_tags(device, file_dept, 'models/trained_model_dept.pth', utterance, num_predictions)
+    tags_cat = predict_tags(device, file_cat, 'models/trained_model_cat.pth', utterance, num_predictions)
+    tags_info = predict_tags(device, file_info, 'models/trained_model_info.pth', utterance, num_predictions)
 
     predicted_tags['ints'] = tags_ints
     predicted_tags['dept'] = tags_dept
