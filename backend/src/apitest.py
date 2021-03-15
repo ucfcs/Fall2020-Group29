ec24183b9c4373e1b860b2be16f89c659fad837a
Ay-ami <60237407+Ay-ami@users.noreply.github.com>
2021-03-15 14:07:44 -0400

    adds "get_questions_via_ID

--- backend/src/apitest.py
+++ backend/src/apitest.py
@@ -14,11 +14,11 @@ from flask import request
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
 from pymongo import ReturnDocument # so that we can return the updated version of the document after updating it
+from bson.objectid import ObjectId
 
 import json
 
 app = Flask(__name__)
-
 app.config['MONGO_DBNAME'] =  'group29' #'ourDB' <-- local connection
 app.config['MONGO_URI'] =  'mongodb+srv://m_user:spell3@clusterg29.pfoak.mongodb.net/group29?retryWrites=true&w=majority' #'mongodb://localhost:27017/ourDB' <-- local connection
 #The "dnspython" module must be installed to use mongodb+srv:
@@ -55,10 +55,10 @@ def return_all( Collection = 'questions' ):
 @app.route('/add_question', methods=['POST']) # add a question to the database with Name, Responses, an Entities, returns this new document
 def add_question( Name = "BS-to-MS", Responses = ["In order to dah da da da da"], Entities = ['BS-to-MS', 'How', 'Sign Up'] ):
   new_question = {'Name': Name, 'Responses': Responses, 'Entities':Entities}
-  # insert_one() doesn't return a document, it returns a result that contains the ObjectID
-  InsertOneResult_Obj = mongo.db.questions.insert_one(new_question) 
-  # append new_question with the ObjectID (as a string) so that we can actually return something that resembles a document :/
-  new_question.update({'_id':str(InsertOneResult_Obj.inserted_id)}) 
+  
+  InsertOneResult_Obj = mongo.db.questions.insert_one(new_question) # insert_one() doesn't return a document, it returns a result that contains the ObjectID
+  
+  new_question.update({'_id':str(InsertOneResult_Obj.inserted_id)}) # append new_question with the ObjectID (as a string) so that we can actually return something that resembles a document :/
  
   return jsonify(new_question)
 
@@ -72,7 +72,10 @@ def get_question( Entities = ['BS-to-MS', 'How', 'Sign Up'] ):
 
   fickleID = found.pop('_id') # jasonify() doens't know how to handle objects of type ObjectID, so we remove it
   found.update({'_id': str(fickleID)}) # put _id back in but as a regular string now
-  
+  print("beep boop")
+  print(__name__)
+
+
   return jsonify(found) #return result as json
 
 # TODO: search questions/responses for a word
@@ -140,6 +143,19 @@ def put_contact(Entities = ['BS-to-MS', 'How', 'Sign Up'], contact = 'heinrich@c
   return jsonify(updated)
 
   # TODO: append an experation date to a question?
+  # TODO: append follow up question 
+# search via ObjectID
+@app.route('/get_question_via_ID', methods=['GET']) # retrive a question based on id
+def get_question_via_ID( _id = "6049969161a11a47b4b8fe4e" ):
+  found = mongo.db.questions.find_one({'_id': ObjectId(_id) }) #finds the entry with the exact set of Entities 
+
+  if (found is None): # if there is no match
+    return jsonify({'result':'no match'})
+
+  fickleID = found.pop('_id') # jasonify() doens't know how to handle objects of type ObjectID, so we remove it
+  found.update({'_id': str(fickleID)}) # put _id back in but as a regular string now
+  
+  return jsonify(found) #return result as json
 
 #delete
 @app.route('/delete_question', methods=['DELETE']) # retrive a question based on Entities
@@ -203,6 +219,7 @@ def update_contact(Name = 'Mark Heinrich', Title = 'CS Advisor', itemToUpdate = 
 # TODO: search a triplet of entities
 
 # TODO: update
+# update all questions with this tag
 
 # TODO: delete
 
