5c467d1659d8f1cc8332d6fe9e4dce8a36af6fd3
Carlos Santiago Bañón <carlossbanon@gmail.com>
2021-03-31 17:15:19 -0400

    Finish fetch_data()

--- backend/src/dataset.py
+++ backend/src/dataset.py
@@ -1,6 +1,7 @@
 from torch.utils.data import Dataset
 from .database_manager import return_all
 import json
+import pandas as pd
 
 class ChatDataset(Dataset):
 
@@ -18,8 +19,38 @@ class ChatDataset(Dataset):
 
 def fetch_data(db, params):
     
+    # Fetch the data from the database.
     raw_data = return_all(db)
 
-    # Script to convert to CSV.
+    data_ints = []
+    data_dept = []
+    data_cat = []
+    data_info = []
 
-    return None
+    # Arrange the training data.
+    with open(raw_data) as json_file:
+
+        data = json.load(json_file)
+
+        for p in data['questions']:
+            for pattern in p['patterns']:
+
+                # Extract the data.
+                data_ints.append([pattern, p['tags'][0]])
+                data_dept.append([pattern, p['tags'][1]])
+                data_cat.append([pattern, p['tags'][2]])
+                data_info.append([pattern, p['tags'][3]])
+
+    # Save training data as DataFrames.
+    df_ints = pd.DataFrame(data_ints, columns =['pattern', 'tag'])
+    df_dept = pd.DataFrame(data_dept, columns =['pattern', 'tag'])
+    df_cat = pd.DataFrame(data_cat, columns =['pattern', 'tag'])
+    df_info = pd.DataFrame(data_info, columns =['pattern', 'tag'])
+
+    # Save training data as CSV files.
+    df_ints.to_csv(params["file_ints"])
+    df_dept.to_csv(params["file_dept"])
+    df_cat.to_csv(params["file_cat"])
+    df_info.to_csv(params["file_info"])
+
+    return True
