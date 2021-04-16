8bc4278efaf250c2c8f870b1408135aa39e1970f
grizzlyThresher <grizzlyThresher@gmail.com>
2021-04-16 19:48:16 -0400

    Creates base files for statistics screen

---
+++ frontend/faculty_system/src/Components/HomeBox/ContentBox/InnerBoxes/StatsBox/StatsBox.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+export class StatsBox extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state={
+            
+        };
+    }
+
+
+    render() {
+        return(
+            <>
+            </>
+        );
+    }
+}
+
+export default StatsBox;
