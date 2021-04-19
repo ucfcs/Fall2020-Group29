b063c7fde77b8d21ad8d3837963a55120cf9bc57
grizzlyThresher <grizzlyThresher@gmail.com>
2021-04-19 13:16:33 -0400

    Stat selection outline

--- frontend/faculty_system/src/Components/HomeBox/ContentBox/InnerBoxes/StatsBox/StatsBox.js
+++ frontend/faculty_system/src/Components/HomeBox/ContentBox/InnerBoxes/StatsBox/StatsBox.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import SelectionBox from '../SelectionBox';
+import {statTypes} from './stats';
+import './statsbox.css';
 
 
 export class StatsBox extends React.Component {
@@ -8,9 +11,13 @@ export class StatsBox extends React.Component {
 
         this.saveCurrent = this.saveCurrent.bind(this);
         this.hasChanges = this.hasChanges.bind(this);
+        this.selectStat = this.selectStat.bind(this);
 
         this.state={
-
+            curStat:{
+                _id:'',
+                name:''
+            }
         };
     }
 
@@ -22,9 +29,38 @@ export class StatsBox extends React.Component {
         return false;
     }
 
+    selectStat(event, stat) {
+        event.preventDefault();
+        this.setState({curStat:stat});
+    }
+
     render() {
         return(
             <>
+                <div id='content-wrapper'>
+                    <div id='stats-selection'>
+                        <div id='stat-type-selection'>
+                            <div className='section-title'>
+                                Stat Type
+                            </div>
+                            <div className='selection-wrapper'>
+                                <SelectionBox 
+                                name='Stat Type'
+                                content={statTypes}
+                                titles={statTypes.map(stat=> ({
+                                    title:stat.name,
+                                    name:''
+                                }))}
+                                update={this.selectStat}
+                                curItem={this.state.curStat}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div id='stat-content-body'>
+
+                    </div>
+                </div>
             </>
         );
     }
---
+++ frontend/faculty_system/src/Components/HomeBox/ContentBox/InnerBoxes/StatsBox/stats.js
@@ -0,0 +1,10 @@
+export const statTypes = [
+    {
+        _id:'1',
+        name:'Unanswered Questions'
+    },
+    {
+        _id:'2',
+        name:'Usage'
+    }
+]
