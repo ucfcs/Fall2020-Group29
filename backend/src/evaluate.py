a56920673e4a50147acd9cd9b9d4a19308fc5591
Carlos Santiago Bañón <carlossbanon@gmail.com>
2021-04-24 21:52:52 -0400

    Begin the evaluation loop.

--- backend/src/evaluate.py
+++ backend/src/evaluate.py
@@ -8,7 +8,24 @@ from dataset import preprocess
 from model import NeuralNet
 import numpy as np
 
-def k_fold_cross_validation(data, params, modifier):
+
+def early_stop(params, loss_list, epoch):
+
+    delta = params['early_stop_delta']
+    patience = params['early_stop_patience']
+
+    # Get the difference of the average losses,
+    curr_avg_loss = np.mean(loss_list[epoch - patience : epoch])
+    prev_avg_loss = np.mean(loss_list[epoch - (2 * patience) : epoch - patience])
+    diff = prev_avg_loss - curr_avg_loss
+
+    if diff < delta:
+        return True, diff
+
+    return False, diff
+
+
+def k_fold_cross_validation(data, params, modifier, verbose=False, graphic=False):
 
     # Set the parameters.
     batch_size = params['batch_size']
@@ -42,13 +59,14 @@ def k_fold_cross_validation(data, params, modifier):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Set up the evaluation metrics.
-    val_loss_list = []
     val_acc_list = []
+    max_epochs_list = []
 
     for fold, (train_index, test_index) in enumerate(kf.split(X, y)):
-
+        
         # Define the model.
         model = NeuralNet(input_size, hidden_size, num_classes).to(device)
+        max_epochs = num_epochs
 
         # Set the loss function and optimizer.
         criterion = nn.CrossEntropyLoss()
@@ -65,6 +83,9 @@ def k_fold_cross_validation(data, params, modifier):
         test = torch.utils.data.TensorDataset(X_test_fold, y_test_fold)
         train_loader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=True)
         test_loader = torch.utils.data.DataLoader(test, batch_size=batch_size, shuffle=True)
+
+        # Set up the evaluation metrics.
+        val_loss_list = []
 
         for epoch in range(num_epochs):
 
@@ -83,87 +104,140 @@ def k_fold_cross_validation(data, params, modifier):
                 optimizer.step()
             
             # Calculate and save the validation metrics.
-            pred = torch.max(model(X_test_fold).data, dim=1)[1]
+            val_output = model(X_test_fold)
+            pred = torch.max(val_output.data, dim=1)[1]
+            val_loss = criterion(val_output, y_test_fold).item()
             val_accuracy = (pred == y_test_fold).sum() / len(y_test_fold)
+            val_loss_list.append(val_loss)
+
+            # Check if early stopping is necessary.
+            stop, diff = early_stop(params, val_loss_list, epoch)
+
+            if stop:
+                print("Stopping early!")
+                max_epochs = epoch
+                break
                 
             # Report epoch metrics.
-            print("Epoch {}/{}\tVal. Loss: {:.4f}\tVal. Accuracy: {:.2f}".format(epoch + 1, num_epochs, loss.item(), val_accuracy))
-       
+            print("Epoch {}/{}\tVal. Loss: {:.4f}\tVal. Accuracy: {:.2f}\tDiff: {:.4f}".format(epoch + 1, num_epochs, val_loss, val_accuracy, diff))
+            
+        max_epochs_list.append(max_epochs)
         val_acc_list.append(float(val_accuracy))
-        print("Final Validation Accuracy: {}".format(val_accuracy))
+
+        # Report the model's metrics.
+        print("\nFinal Validation Accuracy: {}".format(val_accuracy))
+        print("Num. of Epochs for Early Stopping:", max_epochs)
         
-        
-  
-    
     # Report the final metrics.
     print("\nValidation complete.")
     print("Validation Accuracies:", val_acc_list)
     print("Average Validation Accuracy: {:.2f}".format(sum(val_acc_list) / len(val_acc_list)))
+    print("Num. of Epochs:", max_epochs_list)
+    print("Average Num. of Epochs:", int(np.mean(max_epochs_list)))
 
-    # # Plot the training accuracy.
-    # fig = plt.figure()
-    # ax = plt.axes()
-    # x = np.arange(1, num_epochs + 1, 1)
-    # plt.title('Validation Accuracy Per Epoch')
-    # ax.set_xlabel('Epoch')
-    # ax.set_ylabel('Loss')
-    # ax.plot(x, val_acc_list)
-    # plt.show()
-    # return
 
-def evaluate(model, X_test, y_test, tags):
+def evaluate(verbose=False, graphic=False):
 
-    params_file = 'params.json'
+    # Load the configured parameters.
+    params_file = "params.json"
     with open(params_file) as f:
         params = json.load(f)
-    
-    num_predictions = params['num_predictions']
 
-    # Set the device to a GPU if available.
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    FLAGS = params["FLAGS"]
 
-    # Set up as evaluation.
-    model.eval()
+    val_acc_list = []
+    val_loss_list = []
+    val_f1_list = []
+    val_conf_matrix_list - []
 
-    index = 0
-    correct = 0
-    all_preds = []
+    if FLAGS['ints'] == 0 and FLAGS['dept'] == 0 and FLAGS['cat'] == 0 and FLAGS['info'] == 0:
 
-    for pattern in X_test:
+        if verbose:
+            print("No training required.")
+        return
 
-        pattern = pattern.reshape(1, pattern.shape[0])
-        pattern = torch.from_numpy(pattern).to(device)
-        output = model(pattern)
+    if FLAGS['ints'] == 1:
 
-        _, top_predictions = torch.topk(output, num_predictions, dim=1)
-        top_predictions = top_predictions.numpy()[0]
+        file_name = params['file_ints']
+        modifier = 'ints'
+        data = pd.read_csv(file_name)
 
-        # Determine the prediction probabilities.
-        probs = torch.softmax(output, dim=1)
-        probs = probs.detach().numpy()[0]
+        if verbose:
+            print("\nPerforming Cross Validation: Intents")
+        
+        # Train the model.
+        avg_val_acc, avg_val_loss, avg_val_f1_score, avg_val_conf_matrix = k_fold_cross_validation(data, params, modifier, verbose, graphic)
 
-        # Get the top tags.
-        for i in range(top_predictions.size):
-            top_tag = tags[top_predictions[i]]
+        # Save the performance metrics.
+        val_acc_list.append(avg_val_acc)
+        val_loss_list.append(avg_val_loss)
+        val_f1_list.append(avg_val_f1_score)
+        val_conf_matrix_list.append(avg_val_conf_matrix)
 
-        all_preds.append(top_predictions[0])
-            
+    if FLAGS["dept"] == 1:
 
-        if top_predictions[0] == y_test[index]:
-            correct += 1
+        file_name = params["file_dept"]
+        modifier = "dept"
+        data = pd.read_csv(file_name)
 
-        index += 1
+        if verbose:
+            print("\nPerforming Cross Validation: Departments")
+        
+        # Train the model.
+        avg_val_acc, avg_val_loss, avg_val_f1_score, avg_val_conf_matrix = k_fold_cross_validation(data, params, modifier, verbose, graphic)
 
-    accuracy = (correct / len(y_test))
-    conf_mat = confusion_matrix(y_test, all_preds)
-    f1 = f1_score(y_test, all_preds, average='weighted')
+        # Save the performance metrics.
+        val_acc_list.append(avg_val_acc)
+        val_loss_list.append(avg_val_loss)
+        val_f1_list.append(avg_val_f1_score)
+        val_conf_matrix_list.append(avg_val_conf_matrix)
 
+    if FLAGS["cat"] == 1:
 
+        file_name = params["file_cat"]
+        modifier = "cat"
+        data = pd.read_csv(file_name)
 
+        if verbose:
+            print("\nPerforming Cross Validation: Categories")
 
-    print("Correct:", correct)
-    print("Total:", len(y_test))
-    print("Accuracy:", accuracy)
-    print("Confusion Matrix:", confusion_matrix(y_test, all_preds))
+        # Train the model.
+        avg_val_acc, avg_val_loss, avg_val_f1_score, avg_val_conf_matrix = k_fold_cross_validation(data, params, modifier, verbose, graphic)
 
-    return accuracy, conf_mat, f1
+        # Save the performance metrics.
+        val_acc_list.append(avg_val_acc)
+        val_loss_list.append(avg_val_loss)
+        val_f1_list.append(avg_val_f1_score)
+        val_conf_matrix_list.append(avg_val_conf_matrix)
+
+    if FLAGS["info"] == 1:
+
+        file_name = params["file_info"]
+        modifier = "info"
+        data = pd.read_csv(file_name)
+
+        if verbose:
+            print("\nPerforming Cross Validation: Information")
+
+        # Train the model.
+        avg_val_acc, avg_val_loss, avg_val_f1_score, avg_val_conf_matrix = k_fold_cross_validation(data, params, modifier, verbose, graphic)
+
+        # Save the performance metrics.
+        val_acc_list.append(avg_val_acc)
+        val_loss_list.append(avg_val_loss)
+        val_f1_list.append(avg_val_f1_score)
+        val_conf_matrix_list.append(avg_val_conf_matrix)
+
+    # Calculate the average metrics.
+    total_avg_val_acc = np.mean(val_acc_list)
+    total_avg_val_loss = np.mean(val_loss_list)
+    total_avg_f1_score = np.mean(val_f1_list)
+    total_avg_conf_matrix = np.mean(val_conf_matrix_list)
+
+    if verbose:
+        print("\nTotal Average Cross-Validation Accuracy: {:.2f}".format(total_avg_val_acc))
+        print("Total Average Cross-Validation Loss: {:.4f}".format(total_avg_val_loss))
+        print("Total Average Cross-Validation F1 Score: {:.2f}".format(total_avg_f1_score))
+        print("Total Average Cross-Validation Confusion Matrix:", total_avg_conf_matrix)
+
+    return total_avg_val_acc, total_avg_val_loss, total_avg_f1_score, total_avg_conf_matrix
